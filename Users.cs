ea85d54c3d56984c23350f7ca8f84774f7ab498c
SuvamDatta <suvam.datta@ibm.com>
2023-06-01 22:26:48 +0530

    our code

---
+++ Users.cs
@@ -0,0 +1,12 @@
+﻿namespace HackathonWebAPI
+{
+    public class Users
+    {
+        public int UserID { get; set; }
+        public string? Mobile { get; set; }
+        public bool IsServiceProvider { get; set; }
+        public string? Longitude { get; set; }
+        public string? Lantitude { get; set; }
+        public DateTime RegisterDated { get; set; }
+    }
+}
