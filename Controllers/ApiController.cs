355622a16e2514d1f69d8c08e182815b62042740
SuvamDatta <suvam.datta@ibm.com>
2023-06-15 17:26:24 +0530

    changes

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -8,47 +8,54 @@ namespace app.Controllers
     [Route("[controller]")]
     public class ApiController : ControllerBase
     {
-        private readonly ILogger<ApiController> _logger;
-
         public ApiController(ILogger<ApiController> logger)
         {
-            _logger = logger;
         }
 
         [HttpGet(Name = "GetUsers")]
         public IEnumerable<Users> GetUsers()
         {
-            try
-            {
-                Object obj;
-                List<Users> lstUsers = new List<Users>();
-                Users users = new Users();
-                string cstring = "Server=172.21.130.220;Port=3306;User ID=ambiuser;Password=ambi@12345;Database=AmbiDB";
-                using var connection = new MySqlConnection(cstring);
-                connection.Open();
-                using var command = new MySqlCommand("SELECT Top 1 * FROM Users;", connection);
-                using var reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    obj = reader.GetValue(0);
-                    users = (Users)obj;
-                }
-                lstUsers.Add(users);
-                return lstUsers;
-            }
-            catch
-            {
-                List<Users> lstUsers = new List<Users>();
-                Users users = new Users();
-                users.UserID = 1;
-                users.Mobile = "7501197669";
-                users.IsServiceProvider = false;
-                users.Longitude = "88.370210";
-                users.Lantitude = "22.565570";
-                users.RegisterDated = DateTime.Now;
-                lstUsers.Add(users);
-                return lstUsers;
-            }
+            List<Users> lstUsers = new List<Users>();
+            Users users = new Users();
+            users.UserID = 1;
+            users.Mobile = "7501197669";
+            users.IsServiceProvider = false;
+            users.Longitude = "88.370210";
+            users.Lantitude = "22.565570";
+            users.RegisterDated = DateTime.Now;
+            lstUsers.Add(users);
+            return lstUsers;
+            //try
+            //{
+            //    Object obj;
+            //    List<Users> lstUsers = new List<Users>();
+            //    Users users = new Users();
+            //    string cstring = "Server=172.21.130.220;Port=3306;User ID=ambiuser;Password=ambi@12345;Database=AmbiDB";
+            //    using var connection = new MySqlConnection(cstring);
+            //    connection.Open();
+            //    using var command = new MySqlCommand("SELECT Top 1 * FROM Users;", connection);
+            //    using var reader = command.ExecuteReader();
+            //    while (reader.Read())
+            //    {
+            //        obj = reader.GetValue(0);
+            //        users = (Users)obj;
+            //    }
+            //    lstUsers.Add(users);
+            //    return lstUsers;
+            //}
+            //catch
+            //{
+            //    List<Users> lstUsers = new List<Users>();
+            //    Users users = new Users();
+            //    users.UserID = 1;
+            //    users.Mobile = "7501197669";
+            //    users.IsServiceProvider = false;
+            //    users.Longitude = "88.370210";
+            //    users.Lantitude = "22.565570";
+            //    users.RegisterDated = DateTime.Now;
+            //    lstUsers.Add(users);
+            //    return lstUsers;
+            //}
         }
     }
 }
